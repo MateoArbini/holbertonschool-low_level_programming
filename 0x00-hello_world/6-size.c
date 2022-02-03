f510855c0c39524d2e9306c781a6792c30234a2d
MateoArbini <mateoarbini1998@gmail.com>
2022-02-03 09:23:32 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+/**
+ * main - Write a C program that prints the size of various types on the...
+ * Return: always 0
+ *
+ **/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of a int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
