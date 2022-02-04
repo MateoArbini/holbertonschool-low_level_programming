6da8a8255529dee0f266c45b448b7d6ace1c5090
MateoArbini <mateoarbini1998@gmail.com>
2022-02-04 10:02:33 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include "stdio.h"
+
+/**
+ * main - imprimir un numero random de base 10 empezando de 0
+ * Return: always 0
+ **/
+
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++);
+printf("%d", x);
+
+printf("\n");
+return (0);
+}
