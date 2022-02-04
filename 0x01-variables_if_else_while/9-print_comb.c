8ce6c710691ba475c6057d1e2e225fe0a5eec089
MateoArbini <mateoarbini1998@gmail.com>
2022-02-04 11:35:40 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include "stdio.h"
+
+/**
+ * main - prints all possible combinations from 1 digit number
+ * Return: always 0
+ **/
+
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+if (x != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
