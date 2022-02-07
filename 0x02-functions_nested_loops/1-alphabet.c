b6a9bfdd6111c90a969274c27fc188ebf00ff1de
MateoArbini <mateoarbini1998@gmail.com>
2022-02-07 07:16:41 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,4 @@
-#include "stdio.h"
-
-/**
- * main - print aalphabet in lowercase followed by new line
- * Return: Always 0
- **/
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-void print_alphabet(void);
+#include "main.h"
 /**
  * print_alphabet - prints alphabet lowercase
  **/
