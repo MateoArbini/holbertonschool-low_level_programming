939ad88fe8ede4c65db7025a4d36ff3a3942daac
MateoArbini <mateoarbini1998@gmail.com>
2022-02-07 11:13:41 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - imprime todos los numeros hasta el 98
+ *@n: numero ingresado
+ *Return: 0
+ *
+ **/
+void print_to_98(int n);
+{
+printf("Enter value of n: ");
+scanf("%d", &n);
+
+while(n >= 1)
+{
+	printf("%d", n)
+		n++;
+}
+return (0);
+}
+
