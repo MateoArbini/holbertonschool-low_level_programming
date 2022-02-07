2c5d9324d7f6c6e1bd019a54e500f4fb23070aa2
MateoArbini <mateoarbini1998@gmail.com>
2022-02-07 08:35:27 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n);
 {
 if (n > 0)
 {
-_putchar("+, n");
+_putchar("+", n);
 return (1);
 }
 else if(n = 0)
 {
-_putchar("0, n");
+_putchar("0" n);
 return (0);
 }
 else
 {
-_putchar("-, n");
+_putchar("-" n);
 return (-1);
 }
 }
