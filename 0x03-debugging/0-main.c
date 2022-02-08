b13f699a0a71bbff209d1b3f284e4b10929aa72d
MateoArbini <mateoarbini1998@gmail.com>
2022-02-08 08:31:11 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
- * positive_negative - define un numero si es negativo o positivo
+ * positive_or_negative - define un numero si es negativo o positivo
  **/
 
 int main(void)
