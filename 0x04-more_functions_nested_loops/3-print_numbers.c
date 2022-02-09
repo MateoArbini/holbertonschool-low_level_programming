7c2d9ff372c7cfda2f015375ef4c3d8ee49d9f79
MateoArbini <mateoarbini1998@gmail.com>
2022-02-09 08:01:51 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- *print_numbers - imprime del 0 al 9 con salto de linea
+ *print_numbers - imprime del 0 al 9 con salto
  *@x: random number
  **/
 void print_numbers(void)
 {
-int x;
-for (x = '0'; x <= '9'; x++)
-{
-_putchar(x);
-}
-_putchar('\n');
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
 }
