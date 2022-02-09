5cabd1db2ae1608f84bc236838523f82e5e18a22
MateoArbini <mateoarbini1998@gmail.com>
2022-02-09 11:43:46 -0800

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,18 +4,19 @@
  *@n: cantidad de caracteres para la figura
  **/
 
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
 	int a;
 	int b;
 
 	for (a = 0; a < n; a++)
 	{
-		for (b < a; b > 0; b--)
+		for (b = a; b > 0; b--)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
