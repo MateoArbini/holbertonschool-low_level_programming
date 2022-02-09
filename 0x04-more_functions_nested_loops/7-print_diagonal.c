9fecd7c72ff74db503ef2bd3f924916f7b96395f
MateoArbini <mateoarbini1998@gmail.com>
2022-02-09 11:33:52 -0800

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
-*print_diagonal - imprime un triangulo
-*@n: cantidad de \
-**/
-void print_diagonal(int n)
+ *
+ *
+ **/
+
+void print_diagonal(int n);
 {
 	int a;
 	int b;
 
-	if (n <= 0)
-		_putchar('\n');
-
 	for (a = 0; a < n; a++)
 	{
-		for (b < a; b = 0; b++)
-		_putchar(' ');
+		for (b < a; b > 0; b--)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
 		_putchar('\n');
 	}
