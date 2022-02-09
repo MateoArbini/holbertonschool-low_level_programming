73137edbe1ba08e19588789ffbd758d17d9a46c1
MateoArbini <mateoarbini1998@gmail.com>
2022-02-09 09:35:59 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
  *more_numbers - imprime numeros del 1 al 14 10 veces
- *@x: numeros
  **/
 void more_numbers(void)
 {
 	int x;
+	int c;
 
-	for (x = 0; x <= 9; x++)
-	_putchar(x);
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
