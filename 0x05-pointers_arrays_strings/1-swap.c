36dddda1d77b5f51d7349c9376cdbb3bac46b7c3
MateoArbini <mateoarbini1998@gmail.com>
2022-02-11 08:55:58 -0800

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *
- *
+ *swap_int - cambia el valor de pointer a y b entre si
+ *@*a: valor pointer a 
+ *@*b: valor pointer b
  **/
 void swap_int(int *a, int *b)
 {
 	int c = *a;
-	
+
 	*a = *b;
 	*b = c;
 }
