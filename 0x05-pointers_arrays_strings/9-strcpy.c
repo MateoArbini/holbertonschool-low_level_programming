fc1877a6e1575c2731119b48dc87e25b2a364bb8
MateoArbini <mateoarbini1998@gmail.com>
2022-02-11 16:34:14 -0800

    task 8

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,22 +4,15 @@
  *@dest: return value of the point
  *@src: string pointed
  **/
-
 char *_strcpy(char *dest, char *src)
 {
-	delete[] s;
-	s = NULL;
-}
+	int i = 0;
 
-void strcop(string &s1, string s2)
-{
-	delete[] s1;
-	int largo = strlar(s2);
-	s1 = new char [largo + 1];
-	while (s2[i] != '\0')
+	while (str[i] != '\0')
 	{
-		s1[i] = s2[i];
+		_putchar(str[i]);
 		i++;
 	}
-	s1[i] = '\0';
-}
+	 _putchar('\n');
+
+for 
