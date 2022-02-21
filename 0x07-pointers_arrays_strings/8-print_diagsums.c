5bd73f0f6e6fec59580a302ec971b3b35a7cd856
MateoArbini <mateoarbini1998@gmail.com>
2022-02-21 15:17:32 -0800

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums - print the add of diagonal numbers of a matrix square
+ *@a: pointer
+ *@size: var type int
+ **/
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0,
+	int diag1 = 0;
+	int diag2 = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		diag1 += *(a + i);
+		i += size;
+	}
+	for (j = 0; j < size; j++)
+	{
+		diag2 += *(a + ((size - 1) - j));
+		a += size;
+	}
+	printf("%i, %i\n", diag1, diag2);
+}
