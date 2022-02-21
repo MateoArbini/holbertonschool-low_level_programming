d727969ad5dc582c67cba30b4a1c0a9afdb952bf
MateoArbini <mateoarbini1998@gmail.com>
2022-02-21 04:55:29 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,8 @@
  *@b: var type char to fill n times
  *@n: large
  *Return: pointer s
+ *En esta funcion estamos rellenando con la variable b de tipo char,
+ *n cantidad de veces. Luego con int, hacemos el recorrido de n.
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
