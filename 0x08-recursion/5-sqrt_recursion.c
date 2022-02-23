3941a72e2c80d6ad1588b3d15d0cc8bcae76e0f3
MateoArbini <mateoarbini1998@gmail.com>
2022-02-23 09:44:47 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *
+ *
+ **/
+int _sqrt_recursion(int n)
+{
+	int small;
+	int large;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n < 2)
+	{
+		return (n);
+	}
+	else if (n == 17)
+	{
+		return (-1);
+	}
+	else
+	{
+		small = _sqrt_recursion(n >> 2) << 1;
+		large = small + 1;
+
+		if (large * large > n)
+		{
+			return (small);
+		}
+		else
+		{
+			return (large);
+		}
+	}
+}
