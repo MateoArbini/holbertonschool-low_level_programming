eee9d876ca7306be86c1c2ccbdd914c9014db70d
MateoArbini <mateoarbini1998@gmail.com>
2022-02-23 13:19:48 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *
- *
+ *prime - find prime numbers
+ *@y: number
+ *@x: number
+ *Return: 1 if number is prime, 0 otherwise
  **/
 int prime(int y, int x)
 {
@@ -15,6 +17,11 @@ int prime(int y, int x)
 	}
 	return (prime(y, x + 1));
 }
+/**
+ *is_prime_number - if number is prime, return smth
+ *@n: number
+ *Return: 1 if it is prime, 0 otherwise
+ **/
 int is_prime_number(int n)
 {
 	if (n <= 1)
