b34355dd5faf5922291162e3c7938c6771a206ab
MateoArbini <mateoarbini1998@gmail.com>
2022-02-23 05:28:57 -0800

    update main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
@@ -15,3 +17,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 
 int wildcmp(char *s1, char *s2);
+
+int _putchar(char c);
