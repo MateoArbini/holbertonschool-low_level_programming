2b90b0bd8b9f864eb17292b2416dbffba57000c4
MateoArbini <mateoarbini1998@gmail.com>
2022-03-03 07:32:06 -0800

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,5 +8,3 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
-
-int *array_range(int min, int max);
