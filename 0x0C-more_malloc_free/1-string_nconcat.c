86c55996a08f66b6d655c2eab228ce2d8dc9eec8
MateoArbini <mateoarbini1998@gmail.com>
2022-03-03 10:34:13 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1[largo1] != '\0')
 		largo1++;
 
-	s3 = malloc(sizeof(char) * (largo1 + n) + 1);
+	s3 = malloc(sizeof(char) * (largo1 + n + 1));
 
 	if (s3 == NULL)
 		return (NULL);
