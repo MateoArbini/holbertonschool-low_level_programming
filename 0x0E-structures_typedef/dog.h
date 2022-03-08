da7fd6dd87e954bd29f1473ca5c620f92861a285
MateoArbini <mateoarbini1998@gmail.com>
2022-03-08 07:50:45 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog;
+
+typedef struct dog
+{
+	char *name;
+	char *brand;
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
