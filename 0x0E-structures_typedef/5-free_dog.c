c5b2876e478bb338c72616559d33218614aa4957
MateoArbini <mateoarbini1998@gmail.com>
2022-03-08 09:04:39 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - function that frees dogs, related to ex 4
+ *@d: pointer to dog_t
+ **/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d);
+	}
+}
