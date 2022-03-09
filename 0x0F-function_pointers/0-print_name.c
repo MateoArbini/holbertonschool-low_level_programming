6002b4a29451a64b913426fab4933687e55e82c0
MateoArbini <mateoarbini1998@gmail.com>
2022-03-09 06:20:11 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *print_name - function that prints a name
+ *@name: parameter of function
+ *@f: pointer of a function
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+		f(name);
+}
