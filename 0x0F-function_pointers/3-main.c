dd7a8940c25db51bcdb347f98d14ea7832a0f74a
MateoArbini <mateoarbini1998@gmail.com>
2022-03-09 10:18:07 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
+ *main - program that performs simple operations
+ *@argc: argument counter
+ *@argv: arguments given
+ *Return: if conditions are ok, return result, otherwise 98, 99 or 100
  **/
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
@@ -17,19 +19,19 @@ int main (int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
-	
+
 	if (operation == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	if ((argv[2][0] == '/' && argv[3][0] == '0') ||
-		(argv[2][0] == '%' && argv [3][0] == '0'))
+		(argv[2][0] == '%' && argv[3][0] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
