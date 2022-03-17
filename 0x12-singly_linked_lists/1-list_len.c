3894b9c4292c3e6b942c1c78189c1d24e7312b1a
MateoArbini <mateoarbini1998@gmail.com>
2022-03-17 08:50:45 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
- *
- *
+ *list_len - function that returns you the amount of elements
+ *@h: pointer to the first node, of type list_t, which is the type of the
+ *structure
+ *Return: count, which is the amount of elements linked in list_t list
  **/
 size_t list_len(const list_t *h)
 {
