e70d6c2203cb72386677d66ea3484fb1480f0f50
MateoArbini <mateoarbini1998@gmail.com>
2022-03-17 12:56:00 -0700

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,5 @@
  **/
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house
-			upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
