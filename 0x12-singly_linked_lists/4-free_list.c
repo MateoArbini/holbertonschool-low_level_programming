2394921b847a77e18e7bbd00e3be5e2d9607be0a
MateoArbini <mateoarbini1998@gmail.com>
2022-03-17 11:43:42 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *free_list - function that frees a list_t list
+ *@head: pointer to a node
+ **/
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
