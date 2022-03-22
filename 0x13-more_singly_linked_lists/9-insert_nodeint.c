757842e6d76296ded175401ce88dcf3ad1513fe6
MateoArbini <mateoarbini1998@gmail.com>
2022-03-22 07:58:00 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,33 +1,35 @@
 #include "lists.h"
 /**
- *insert_nodeint_at_index - function that inserts a
- *new node at a given position
- *@head: pointer that points to the head node of the list
- *@idx: is the index of the list where the new node should be added. Index
- *starts at 0
- *@n: var type int of each node
- *Return: the address of the new node, or NULL if it failed
+ *
+ *
  **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	struct listint_s *temp2 = (struct listint_s *) malloc(sizeof(listint_t));
-
-	struct listint_s *temp = *head;
-
-	temp2->n = n;
-	temp2->next = NULL;
-	if (temp2 == NULL)
+	listint_t *aux = *head;
+	unsigned int size = 0;
+	
+	if (head == NULL)
 	{
 		return (NULL);
 	}
 
-	while (idx != 1)
+	if (idx < 1 || idx > size + 1)
 	{
-		temp = temp->next;
-		idx--;
+		return (NULL);
 	}
-	temp2->next = temp->next;
-	temp->next = temp2;
-
-	return (temp2);
-}
+	while (idx--)
+	{
+		if (idx == 0)
+		{
+			aux->next = *head;
+			*head = aux;
+			aux->n = n;
+		}
+		else
+		{
+			head = &(*head)->next;
+		}
+		size++;
+	}
+	return (aux);
+}		
