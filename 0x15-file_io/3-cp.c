97c45f1086df0acd5e2f9900fc53e7e1612248e2
MateoArbini <mateoarbini1998@gmail.com>
2022-03-28 16:13:54 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *main - fklsndf
+ *@argc: kfjf
+ *@argv: psofsd
+ *Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+int fd_filefrom, fd_fileto, write_validator = 0;
+char *buffer;
+int read_length = 0, close1_validator = 0, close2_validator = 0;
+
+if (argc != 3)
+dprintf(STDERR_FILENO, "Usage: cp_file_from file_to\n"), exit(97);
+fd_filefrom = open(argv[1], O_RDONLY);
+if (fd_filefrom == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+}
+fd_fileto = open(argv[2], O_CREAT | O_RDONLY | O_WRONLY | O_TRUNC, 0664);
+if (fd_fileto == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+buffer = malloc(sizeof(char *) * 1024);
+if (buffer == NULL)
+	return (0);
+read_length = read(fd_filefrom, buffer, 1024);
+if (read_length == -1)
+{
+free(buffer);
+dprintf(STDERR_FILENO, "Error: Can't read from file\n"), exit(98);
+}
+if (read_length > 0)
+	write_validator = write(fd_fileto, buffer, read_length);
+if (write_validator == -1)
+{
+free(buffer);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+}
+close1_validator = close(fd_filefrom);
+if (close1_validator == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n"), exit(100);
+close2_validator = close(fd_fileto);
+if (close2_validator == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n"), exit(100);
+close(fd_filefrom);
+close(fd_fileto);
+return (0);
+}
+
+
+
