d889bf6b89dcdaf81204a33b5a4c7389937e6a5f
MateoArbini <mateoarbini1998@gmail.com>
2022-05-05 08:06:23 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,8 +12,17 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
+		if (h->n == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("%d\n", h->n);
+		}
 		h = h->next;
 		count++;
 	}
-	return (count);
+	return (count);	
 }
