68110cd98a58c8e715e4a5c681fef037123da48b
MateoArbini <mateoarbini1998@gmail.com>
2022-05-05 08:51:38 -0700

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *free_dlistint_t - function that frees a list
+ *@head: pointer to a head
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	void *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
