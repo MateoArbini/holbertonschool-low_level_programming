324167b96353ec1773879eb6bbcacd1c967b894d
MateoArbini <mateoarbini1998@gmail.com>
2022-05-05 10:39:23 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 		return (NULL);
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 	{
 		free(newnode);
 		return (NULL);
