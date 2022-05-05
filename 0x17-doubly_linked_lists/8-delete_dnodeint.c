c1d7328f7297ec5081f912221f1e29aeb88f48a8
MateoArbini <mateoarbini1998@gmail.com>
2022-05-05 10:06:19 -0700

    Task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index - function that delets a node at certain position
+ *@head: pointer that points to the header
+ *@index: index
+ *@Return 1
+ **/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *aux;
+	dlistint_t *next;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	aux = *head;
+
+	if (index == 0)
+	{
+		*head = aux->next;
+		free(aux);
+		return (1);
+	}
+	for (; aux != NULL && i < index - 1; i++)
+		aux = aux->next;
+
+	if (aux == NULL || aux->next == NULL)
+		return (-1);
+
+	next = aux->next->next;
+	free(aux->next);
+	aux->next = next;
+
+	return (1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,5 +31,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 void sumOfNodes(dlistint_t *head, int *sum);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
