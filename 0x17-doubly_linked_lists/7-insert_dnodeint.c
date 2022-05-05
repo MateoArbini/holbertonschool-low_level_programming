fe468f1a6c979309d11a81cd92814c0af202d565
MateoArbini <mateoarbini1998@gmail.com>
2022-05-05 09:37:25 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,6 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	newnode->next = aux->next;
+	newnode->prev = aux->prev;
 	aux->next = newnode;
 	return (newnode);
 }
