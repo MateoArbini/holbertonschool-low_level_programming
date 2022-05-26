0cef74b2ddc7c899a207bb462e73623685bd26f0
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 08:30:10 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash = NULL;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+		return (NULL);
+
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
+	}
+
+	return (new_hash);
+}
