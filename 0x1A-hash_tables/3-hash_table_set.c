32d4eb97bc9fcbfb9925dd33b6a6414375887251
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 11:42:31 -0700

    task 3 with comments

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,41 +3,43 @@
 /**
  * hash_table_set - function that adds an element to the hash table
  * @ht: hash table that you want to add or update
- * @key: is the key, can not be an empty string
- * @value: is the value associated with the key
- * Return: 1 if it succeeded or 0 otherwise
+ * @key: cant be an empty string
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *search_nod = NULL;
-	hash_node_t *new_nod = NULL;
+	hash_node_t *search_node = NULL;
+	hash_node_t *new_node = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 
-	new_nod = malloc(sizeof(hash_node_t));
-	if (new_nod == NULL)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-	search_nod = ht->array[index];
-	while (search_nod != NULL)
+
+	search_node = ht->array[index];
+
+	while (search_node != NULL)
 	{
-		if (strcmp(search_nod->key, key) == 0)
+		if (strcmp(search_node->key, key) == 0)
 		{
-			free(search_nod->value);
-			search_nod->value = strdup(value);
-			free(new_nod);
+			free(search_node->value);
+			search_node->value = strdup(value);
+			free(new_node);
 			return (1);
 		}
-		search_nod = search_nod->next;
+		search_node = search_node->next;
 	}
 
-	new_nod->key = strdup(key);
-	new_nod->value = strdup(value);
-	new_nod->next = ht->array[index];
-	ht->array[index] = new_nod;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
