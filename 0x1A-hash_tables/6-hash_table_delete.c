22b9f78e52ac5e6721216d3f2b4717d87472682c
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 14:46:00 -0700

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,5 +8,39 @@
  **/
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *node = NULL;
+	hash_node_t *aux = NULL;
 
+	if (ht == NULL)
+	{
+		return;
+	}
+	if (ht->array == NULL)
+	{
+		free(node);
+		return;
+	}
+	while (i < ht->size)
+	{	
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			aux = node->next;
+			if (node->key != NULL)
+			{
+				free (node->key);
+			}
+			if (node->value != NULL)
+			{
+				free (node->value);
+			}
+			free(node);
+			node = aux;
+		}
+		i++;
+		free(node);
+	}
+	free(ht->array);
+	free(ht);
 }
