943620bceda7fa18fa76f690714cdaea139e78a0
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 09:03:04 -0700

    task 0 with comments and betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	
+
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
