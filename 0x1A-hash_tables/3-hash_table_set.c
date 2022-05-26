e7951fdc8aaa583661558293aa570a6129eb9071
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 09:21:16 -0700

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - function that adds an element to the hash table
+ *@key: is the key and it cant be a string
+ *@ht: hash table we want to add or update the key/value
+ *@value: value associated with the key. Must be duplicated and can be a string
+ *Return: 1 if succeded and 0 otherwise
+ **/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
