f6a3250d41bc2f9dbd4fd2aa498cb9ac1e560cd0
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 13:42:16 -0700

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ *hash_table_delete - function that deletes a hash table
+ *@ht: hash table to delete
+ **/
+void hash_table_delete(hash_table_t *ht)
+{
+
+}
