d38e21911bdcd45b1cc6515845e531af1152e288
MateoArbini <mateoarbini1998@gmail.com>
2022-05-26 20:15:11 -0700

    task 6 with betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,18 +22,18 @@ void hash_table_delete(hash_table_t *ht)
 		return;
 	}
 	while (i < ht->size)
-	{	
+	{
 		node = ht->array[i];
 		while (node != NULL)
 		{
 			aux = node->next;
 			if (node->key != NULL)
 			{
-				free (node->key);
+				free(node->key);
 			}
 			if (node->value != NULL)
 			{
-				free (node->value);
+				free(node->value);
 			}
 			free(node);
 			node = aux;
