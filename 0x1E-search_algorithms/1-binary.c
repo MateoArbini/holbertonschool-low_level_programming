054898892706eb39f2f7ac748da50b096642a39b
Mateo <mateoarbini1998@gmail.com>
2022-08-24 22:22:21 +0000

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - function that search for a value stored in a sorted array
+ * of ints using the binary search algorithm
+ * @array: pointer to the first element or the array
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+}
